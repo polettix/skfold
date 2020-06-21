db9cfdb07b420f816d297ee5ca6668957da1deed
Flavio Poletti <flavio@polettix.it>
2020-06-21 17:03:30 +0200

    Advance...

--- dot-skfold/modules/dibs/custom.pm
+++ dot-skfold/modules/dibs/custom.pm
@@ -4,19 +4,47 @@ use warnings;
 
 sub adapt_module_configuration {
    my ($config) = @_;
+
+   return _templates_list($config) if $config->{opts}{templates};
+
+   my $source = $config->{opts}{template};
+   main::LOGDIE "select one template with -t (call with -T for list)"
+      unless defined $source;
+
    my $spec = $config->{files}[0];
-   my $target = path($config->{target})->absolute;
+   my ($target, @files);
+   if ($config->{target} eq '-') {
+      $target = '-';
+   }
+   else {
+      $target = path($config->{target})->absolute;
+      @files = map {
+         my %record = (destination => $_);
+         $record{mode} = $spec->{dmode} if defined $spec->{dmode};
+         \%record;
+      } ancestors_for($target);
+   }
+   push @files, {%$spec, destination => $target, source => $source};
+   $config->{files} = \@files;
+
    $config->{target_dir} = path('/');
 
-   my @files = map {
-      my %record = (destination => $_);
-      $record{mode} = $spec->{dmode} if defined $spec->{dmode};
-      \%record;
-   } ancestors_for($target);
-
-   push @files, {%$spec, destination => $target};
-   $config->{files} = \@files;
    return;
 };
+
+sub _templates_list {
+   my $config = shift;
+   my $list = join "\n",
+      grep {! /^\./}
+      map {$_->basename}
+      $config->{templates_dir}->children;
+   $list .= "\n";
+   $config->{files} =[
+      {
+         source => sub { return $list },
+         destination => '-',
+      }
+   ];
+}
 
 1;
